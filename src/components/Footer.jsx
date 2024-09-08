97e66f530ca232b5e96dfd0ce2a13918f88cee74
Krizdaniels <dannychris2@gmail.com>
2024-09-08 14:21:46 +0100

    Ecommerce Frontend

---
+++ src/components/Footer.jsx
@@ -0,0 +1,145 @@
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@material-ui/icons";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  display: flex;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 10px 0px;
+  font-size: 14px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 10px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ display: "none" })}
+`;
+
+const Title = styled.h3`
+  margin-block-end: 30px;
+  font-size: 20px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
+
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  margin-block-start: 10px;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>SHOEVILLE STORE.</Logo>
+        <Desc>
+        Welcome to Shoeville Store, an innovative online marketplace designed to offer a wide range of footwear, Clothes, Bags, Jackets, etc, from well-known brands as well as unique, handmade products. 
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{marginRight:"10px"}}/> 149 Club Road, Lagos Street
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{marginRight:"10px"}}/> +234 816 734 5039
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{marginRight:"10px"}} /> contact@shoeville21@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/zhR5vn6/new-arrivals.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 10% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/PwLxbvg/lady1.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/jvJVmKP/lady-2.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/yyFq774/White-and-Black-Fashion-Instagram-Story-1.png",
+      title: "GEN Z COLLECTION!",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/X55BRrJ/Gray-Professional-Shoe-and-Sneaker-Restoration-Instagram-Post.png",
+      title: "SNEAKER RESTORE",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/vjLWtZH/Beige-Black-Minimalist-This-Or-That-Bag-Edition-Instagram-Post.png",
+      title: "LEATHER WEARS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://i.ibb.co/mNLMPXg/air-jordan-shoe-nike-sneakers-adidas-yeezy-jordan-9d493e6896549a636419ee58d67ba6c0.png",
+    },
+    {
+      id:2,
+      img:"https://i.ibb.co/HggS2v0/adidas-yeezy-sneakers-nike-air-yeezy-adidas-c26fba4fedc7ac52f11c4141830ab29c.png",
+    },
+    {
+      id:3,
+      img:"https://i.ibb.co/9HkXX1H/nike-air-max-nike-air-yeezy-adidas-yeezy-shoe-nike-16e4c1042d96210e6699f03a28751226.png",
+    },
+    {
+      id:4,
+      img:"https://i.ibb.co/pd1DNhG/adidas-shoe-football-boot-cleat-ace-9587a4d2743ceb6c09a37b864c3df3df.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://i.ibb.co/D5Gj9c1/t-shirt-hoodie-sleeve-sweater-polo-shirt-66a9f43bd20ccd8ba5b2c7d2b21fa651.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
